7ecf07944bc1456fd0c860b9b4e92e4289aa0985
juicecultus <juicecultus@gmail.com>
2020-04-27 23:53:44 +0100

    refactoring code & build custom hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem('counterState');
-  if (storage) return JSON.parse(storage).count;
-  return { count: 0 };
-};
-
 const storeStateInLocalStorage = (count) => {
   localStorage.setItem('counterState', JSON.stringify({ count }));
   console.log(localStorage);
 };
 
+const useLocalStorage = (initialState, key) => {
+  const get = () => {
+    const storage = localStorage.getItem(key);
+    console.log(localStorage, storage);
+    if (storage) return JSON.parse(storage).value;
+    return initialState;
+  };
+
+  const [value, setValue] = useState(get());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify({ value }));
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const Counter = ({ max, step }) => {
-  const [count, setcount] = useState(getStateFromLocalStorage());
+  const [count, setCount] = useLocalStorage(0, 'count');
 
   const increment = () => {
-    setcount((c) => {
+    setCount((c) => {
       if (c >= max) return c;
       return c + step;
     });
   };
-  const decrement = () => setcount(count - 1);
-  const reset = () => setcount(0);
+
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
 
   useEffect(() => {
+    // Click "open in a new window" to see the title change
     document.title = `Counter: ${count}`;
   }, [count]);
 
